eb126082ad1b821f9bb1be899f05fe65010ca45b
Reuben Beeler <reubenfbeeler@icloud.com>
2020-04-19 15:57:50 -0700

    Update HangMan.java

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,11 +1,14 @@
 package _04_HangMan;
 
 import java.util.Stack;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -13,17 +16,21 @@ import javax.swing.JPanel;
 import javax.swing.JOptionPane;
 import javax.swing.JLabel;
 
-public class HangMan {
+public class HangMan implements KeyListener {
 	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	
 	int wordCount;
-	String dictionaryName = "_04_HangMan_Instructions.html";
+	String dictionaryName = "dictionary.txt";
 	int dictionaryLength = Utilities.getTotalWordsInFile(dictionaryName);
 	
-	String[] word;
+	ArrayList<String> alphabet = new ArrayList<String>(Arrays.asList(new String[] {"a", "b", "c", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}));
+	ArrayList<String> answer;
+	String[] letters;
+	boolean dead = false;
+	boolean end = false;
 	
 	ArrayList<String> arrayList = new ArrayList<String>();
 	Stack<String> stack = new Stack<String>();
@@ -38,6 +45,7 @@ public class HangMan {
 	
 	void setup() {
 		frame.setSize(1000, 1000);
+		frame.addKeyListener(this);
 		frame.add(panel);
 		panel.add(label);
 		
@@ -69,18 +77,63 @@ public class HangMan {
 			arrayList.remove(stack.get(stack.size() - 1));
 		}
 		
-		word = new String[stack.pop().length()];
-		for (int i = 0; i < word.length; i++) {word[i] = "_";}
+		answer = new ArrayList<String>(Arrays.asList(stack.pop().split("")));
+		for (int i = 0; i < answer.size(); i++) {letters[i] = "_";}
+		
+		setLabel();
+		
+		/*
+		if (dead) {JOptionPane.showMessageDialog(frame, "You Lose");}
+		else {JOptionPane.showMessageDialog(frame, "YOU WIN");}
+		*/
+	}
+	
+	void finish() {
 		
 	}
 	
-	void changeLabel() {
-		String text = "";
-		for (int i = 0; i < word.length; i++) {
-			text += word[i] + " ";
+	void update(String letter) {
+		
+		int change = 0;
+		while (answer.contains(letter)) {
+			letters[answer.indexOf(letter)] = letter;
+			change += 1;
 		}
-		label.setText(text);
-		frame.setVisible(true);
+		if (change == 0) {/*Take off one life*/}
+		
+		//Show letters guessed on a separate JLabel
+		
+		if (!setLabel().contains("_") || dead) {
+			end = true;
+			finish();
+		}
 	}
 	
+	String setLabel() {
+		String word = "";
+		for (String letter : letters) {word += letter + " ";}
+		label.setText(word);
+		frame.setVisible(true);
+		return word;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if (!end) {
+			String letter = KeyEvent.getKeyText(e.getKeyCode()).toLowerCase();
+			if (alphabet.contains(letter)) {update(letter);}
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
